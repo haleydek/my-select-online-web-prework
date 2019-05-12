88973d088d0e4ae5175b6c34defb0b2173dbf1b6
Haley DeKeyser <hcd09@live.com>
2019-05-12 03:53:33 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -6,7 +6,7 @@ def my_select(array)
    
    while i < array.size
       if true
-      new_array << yield(array[i])
+        new_array << yield(array[i])
       i += 1
       else i += 1
       end
