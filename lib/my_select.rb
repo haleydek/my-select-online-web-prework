33ff316bfa29dcf5c6193d354db021548e438c58
Haley DeKeyser <hcd09@live.com>
2019-05-12 03:50:17 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+ if block_given?
+   
+   i = 0
+   new_array = []
+   
+   while i < array.size
+    if true
+      new_array << yield(array[i])
+      i += 1
+    else i += 1
+    end
+    
+    new_array
+   
+ else
+   "No block given."
+ end
 end
